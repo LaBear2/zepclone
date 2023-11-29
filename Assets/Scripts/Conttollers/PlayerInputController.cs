efd79411d0d0db5634af1a82d41e5265236f1933
LaBear2 <op9339291@gmail.com>
2023-11-29 10:56:56 +0900

    캐릭터 회전없앰 및 움직임

--- Assets/Scripts/Conttollers/PlayerInputController.cs
+++ Assets/Scripts/Conttollers/PlayerInputController.cs
@@ -11,13 +11,13 @@ public class PlayerInputController : TopDownCharacterController
         _camera = Camera.main;
     }
 
-    public void onMove(InputValue value)
+    public void OnMove(InputValue value)
     {
         Vector2 moveInput = value.Get<Vector2>().normalized;
         CallMoveEvent(moveInput);
     }
 
-    public void onLook(InputValue value)
+    public void OnLook(InputValue value)
     {
         Vector2 newAim = value.Get<Vector2>();
         Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
